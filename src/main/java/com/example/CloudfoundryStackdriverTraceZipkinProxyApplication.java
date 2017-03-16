222c46f76cff0c30f7b6982a35917ea4c5adbfe9
Josh Long <josh@joshlong.com>
2017-03-15 22:49:06 -0500

    ic

---
+++ src/main/java/com/example/CloudfoundryStackdriverTraceZipkinProxyApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudfoundryStackdriverTraceZipkinProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudfoundryStackdriverTraceZipkinProxyApplication.class, args);
+	}
+}
